6359a9863d076ab942dd6a092023e5707354a087
michealdayo64 <omotoshomicheal93@gmail.com>
2026-03-27 10:28:50 +0100

    react vite debugging

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta, timezone
+from lib.db import pool, query_wrap_object, query_wrap_array
 #from opentelemetry import trace
 
 #tracer = trace.get_tracer("home.activities")
@@ -10,7 +11,31 @@ class HomeActivities:
         #span = trace.get_current_span()
         now = datetime.now(timezone.utc).astimezone()
         #span.set_attribute("app.now", now.isoformat())
-        results = [{
+        sql = query_wrap_array("""
+            SELECT
+                activities.uuid,
+                users.display_name,
+                users.handle,
+                activities.message,
+                activities.replies_count,
+                activities.reposts_count,
+                activities.likes_count,
+                activities.reply_to_activity_uuid,
+                activities.expires_at,
+                activities.created_at
+            FROM public.activities
+            LEFT JOIN public.users ON users.uuid = activities.user_uuid
+            ORDER BY activities.created_at DESC
+            """)
+        print(sql)
+        with pool.connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql)
+                # this will return a tuple
+                # the first field being the data
+                json = cur.fetchone()
+        return json[0]
+        '''results = [{
             'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
             'handle':  'Andrew Brown',
             'message': 'Cloud is fun!',
@@ -62,4 +87,4 @@ class HomeActivities:
             }
             results.insert(0,extra_crud)
         #span.set_attribute("app.result_length", len(results))
-        return results
+        return results'''

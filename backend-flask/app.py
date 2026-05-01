79712b06ea467ed9773c4df416f02218d3e6aed2
unknown <Loshai@HELOSHALTIT1357.ADLOSHA.local>
2026-05-01 21:14:02 +0100

    Dynamodb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -24,8 +24,8 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor'''
 
 # AWS X-Ray
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+#from aws_xray_sdk.core import xray_recorder
+#from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 # CloudWatch
 #import watchtower
@@ -51,10 +51,11 @@ tracer = trace.get_tracer(__name__)'''
 
 app = Flask(__name__)
 app.run(host="0.0.0.0", port=5000, debug=True)
+app.logger.setLevel("DEBUG")
 # AWS X-Ray -----
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+#xray_url = os.getenv("AWS_XRAY_URL")
+#xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
+#XRayMiddleware(app, xray_recorder)
 
 
 # Cognito JWT Token
@@ -169,14 +170,16 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 #@xray_recorder.capture('activities_home')
 def data_home():
-    print("hello")
+    
+    app.logger.debug("Hello")
     access_token = extract_access_token(request.headers)
+    app.logger.debug(f"Access token: {access_token}")
     try:
         claims = cognito_jwt_token.verify(access_token)
         # authenicatied request
         app.logger.debug("authenicated")
         app.logger.debug(claims)
-        app.logger.debug(claims['username'])
+        app.logger.debug(claims.get('sub'))
         data = HomeActivities().run(cognito_user_id=claims['username'])
     except TokenVerifyError as e:
         # unauthenicatied request
@@ -194,7 +197,7 @@ def data_notifications():
 
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
-@xray_recorder.capture('activities_users')
+#@xray_recorder.capture('activities_users')
 def data_handle(handle):
     model = UserActivities.run(handle)
     if model['errors'] is not None:
@@ -220,10 +223,11 @@ def data_activities():
     #access_token = extract_access_token(request.headers)
     #claims = cognito_jwt_token.verify(access_token)
     #app.logger.debug(claims)
-    user_handle = "michealdayo64"
+    user_handle = request.json['username']
     message = request.json['message']
     ttl = request.json['ttl']
 
+    app.logger.debug(user_handle)
     app.logger.debug(message)
     app.logger.debug(ttl)
     
